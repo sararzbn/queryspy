d669627dc4ac892be44ec4041ab99f8cb14a711a
Sara Rouzbahani <srouzbahani35@gmail.com>
2025-05-18 19:51:43 +0200

    feat: add query listener for slow SQL and log them to queryspy.log
    - Added config file with customizable threshold
    - Registered DB::listen() for slow query logging
    - Created custom log channel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/test-slow', function () {
+    DB::select('SELECT pg_sleep(1)');
+    return 'done';
 });
